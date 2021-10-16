3ce47a6e262568f714786992ebef4960e7f98801
Rugnus <sungur.gasanov@mail.ru>
2021-10-16 16:30:29 +0300

    Initial Commit

---
+++ todoApp/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by Sungur on 16.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
